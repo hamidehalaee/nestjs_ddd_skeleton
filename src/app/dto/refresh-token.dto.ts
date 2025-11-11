e9f96ed157aae60c8bb82c07330bb00369331b7b
h.alaee <alaee.work@gmail.com>
2025-11-11 15:25:47 +0330

    retest pushing code correctly

--- src/app/dto/refresh-token.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
-export class RefreshTokenDto {
-  @ApiProperty({ example: 'string', description: 'Refresh token' })
-  @IsString()
-  refreshToken: string;
-}
